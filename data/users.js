eb3e33cbd56b47ce7cdedc0752023f49b88c632e
Soon-Mi Sugihara <ssoonmi@gmail.com>
2020-03-18 15:29:27 -0700

    Allow automatic seeding to Atlas using Javascript arrays

---
+++ data/users.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    name: "Soon-Mi"
+  },
+  {
+    name: "Dean"
+  },
+  {
+    name: "Mike"
+  },
+  {
+    name: "Tommy"
+  },
+  {
+    name: "Mashu"
+  },
+  {
+    name: "Alvin"
+  }
+];
